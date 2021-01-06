dd6c1692e6c95dacc04b73466cbb64a9286ccb1a
katiePDX <61799774+katiepdx@users.noreply.github.com>
2021-01-05 18:27:04 -0800

    add mondrian copy styles

--- src/App.js
+++ src/App.js
@@ -1,23 +1,19 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      {/* 10 divs */}
+      <div className="big-red">Big Red</div>
+      <div className="sm-yellow">Small Yellow</div>
+      <div className="white-square">White Square</div>
+      <div className="sm-red">Small Red</div>
+      <div className="lg-vert-white">Large vertical White</div>
+      <div className="horizontal-blue">Horizontal Blue</div>
+      <div className="sm-vert-white">Small vertical White</div>
+      <div className="lg-horizontal-white">Lg Horizontal White</div>
+      <div className="vert-blue">Vertical blue</div>
+      <div className="yellow-square">Yellow square</div>
     </div>
   );
 }
